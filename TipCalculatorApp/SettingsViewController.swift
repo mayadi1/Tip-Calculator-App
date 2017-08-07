fd0a89604d7ca26c079b659ff0fa8e725c79b39e
Mohamed Ayadi <admin@ayadios.com>
2017-08-07 16:22:37 -0700

    setting screen added

---
+++ TipCalculatorApp/SettingsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SettingsViewController.swift
+//  TipCalculatorApp
+//
+//  Created by Mohamed Ayadi on 8/7/17.
+//  Copyright © 2017 com.ayadios. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet var percentageAmountsTextFields: [UITextField]!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.hideKeyboard()
+        let defaults = UserDefaults.standard
+        let arrayValue = defaults.object(forKey: "tipPercentage") as! [Double]
+
+        for i in 0..<3 {
+            percentageAmountsTextFields[i].text = "\(arrayValue[i])"
+        }
+    }
+
+    @IBAction func updateBtnPressed(_ sender: Any) {
+        var tipPercentage = [Double]()
+        let defaults = UserDefaults.standard
+        
+        for i in 0..<3 {
+            tipPercentage.append(Double(percentageAmountsTextFields[i].text!)!)
+        }
+        defaults.set(tipPercentage, forKey: "tipPercentage")
+        defaults.synchronize()
+    }
+}
--- TipCalculatorApp/TipViewController.swift
+++ TipCalculatorApp/TipViewController.swift
@@ -9,18 +9,30 @@
 import UIKit
 
 class TipViewController: UIViewController{
-
+    
     @IBOutlet var tipResults: [UILabel]!
     @IBOutlet weak var billTextField: UITextField!
-    var tipPercentage: [Double] = [0.15,0.20,0.25]
-    
+    var tipPercentage: [Double] = [15,20,25]
+    var customTipPercentLabelValue : Double = 30
     @IBOutlet weak var customTipPercentLabel: UILabel!
     @IBOutlet weak var customTipResultLabel: UILabel!
+    @IBOutlet var tipPercentLabels: [UILabel]!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let defaults = UserDefaults.standard
+        if(defaults.object(forKey: "tipPercentage") as? [Double] != nil){
+            tipPercentage = (defaults.object(forKey: "tipPercentage") as? [Double])!
+            setTipPercentLables()
+        }else{
+            setTipPercentLables()
+            defaults.set(tipPercentage, forKey: "tipPercentage")
+            defaults.synchronize()
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         self.hideKeyboard()
         billTextField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
-
     }
     
     func textFieldDidChange(_ textField: UITextField) {
@@ -30,19 +42,30 @@ class TipViewController: UIViewController{
     func updateTipResults(){
         if(!(billTextField.text?.isEmpty)!){
             for i in 0..<3 {
-                tipResults[i].text = "\((Double(billTextField.text!)! * tipPercentage[i]).roundTo(places: 2))"
+                tipResults[i].text = "$\((Double(billTextField.text!)! * (tipPercentage[i] / 100)).roundTo(places: 2))"
+                customTipResultLabel.text = "$\((Double(billTextField.text!)! * (customTipPercentLabelValue / 100)).roundTo(places: 2))"
             }
         }else{
             for i in 0..<3 {
                 tipResults[i].text = "0"
+                customTipResultLabel.text = "$0"
             }
         }
     }
     
     @IBAction func sliderValueChanged(sender: UISlider) {
         let currentValue = Double(sender.value).roundTo(places: 2)
-        customTipPercentLabel.text = "\(currentValue)"
-        customTipResultLabel.text = "\((Double(billTextField.text!)! * currentValue).roundTo(places: 2))"
+        customTipPercentLabel.text = "\(currentValue)%"
+        self.customTipPercentLabelValue = currentValue
+        if(!(billTextField.text?.isEmpty)!){
+            customTipResultLabel.text = "$\((Double(billTextField.text!)! * (currentValue / 100)).roundTo(places: 2))"
+        }
+    }
+    
+    func setTipPercentLables(){
+        for i in 0..<3 {
+            tipPercentLabels[i].text = "\(tipPercentage[i])%"
+        }
     }
 }
 
