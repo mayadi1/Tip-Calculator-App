089584babc7f157e38358fd6b88a75a3286bb042
Mohamed Ayadi <admin@ayadios.com>
2017-08-07 13:30:00 -0700

    UI set

--- TipCalculatorApp/ViewController.swift
+++ TipCalculatorApp/ViewController.swift
@@ -8,18 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TipViewController: UIViewController, UITextFieldDelegate{
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print("yes")
+        
     }
-
-
 }
 
