de757e250fe076b858a29ae93e12a490008163a4
Mohamed Ayadi <admin@ayadios.com>
2017-08-08 10:16:09 -0700

    tipCalculatorWalkthrough gif added

---
+++ TipCalculatorApp/Double.swift
@@ -0,0 +1,17 @@
+//
+//  Double.swift
+//  TipCalculatorApp
+//
+//  Created by Mohamed Ayadi on 8/7/17.
+//  Copyright © 2017 com.ayadios. All rights reserved.
+//
+
+import Foundation
+
+extension Double {
+    /// Rounds the double to decimal places value
+    func roundTo(places:Int) -> Double {
+        let divisor = pow(10.0, Double(places))
+        return (self * divisor).rounded() / divisor
+    }
+}
